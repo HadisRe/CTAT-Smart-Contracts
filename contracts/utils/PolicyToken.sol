1c5c7f44ee38535a7e2f7f59b381edfc684e9612
Hadis Rezaei <hrezaei@unisa.it>
2025-06-04 21:16:45 +0200

    Update PolicyToken.sol

--- contracts/utils/PolicyToken.sol
+++ contracts/utils/PolicyToken.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./AccessToken.sol"; // اطمینان حاصل کنید که مسیر صحیح فایل AccessToken.sol را تنظیم کرده‌اید
-
-import "./PatientsRegistry.sol"; // اطمینان حاصل کنید که مسیر صحیح فایل AccessToken.sol را تنظیم کرده‌اید
+import "./AccessToken.sol"; // Make sure to set the correct path for AccessToken.sol file
+import "./AccessType.sol"; // Make sure to set the correct path for AccessType.sol file
  
 contract PolicyToken {
-    // ساختار برای نگهداری سیاست دسترسی بیمار به دارو
+    // Structure for storing patient drug access policy
     struct Policy {
         uint256 id;
         address patientAddress;
@@ -17,26 +16,26 @@ contract PolicyToken {
     uint256 private currentPolicyId;
     mapping(uint256 => Policy) public policies;
 
-    // مالک قرارداد برای مدیریت کلی
+    // Contract owner for general management
     address public owner;
 
-    // تنظیم مالک در کانستراکتور
+    // Set owner in constructor
     constructor() {
         owner = msg.sender;
     }
 
-    // Modifier برای اطمینان از اینکه تنها مالک می‌تواند عملیات‌های خاصی را انجام دهد
+    // Modifier to ensure only owner can perform specific operations
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this action");
         _;
     }
 
-    // تابعی برای ایجاد یک سیاست جدید
+    // Function to create a new policy
     function createPolicy(address _patientAddress, uint256 _drugId, AccessType _accessType) public onlyOwner returns (uint256) {
         currentPolicyId++;
         uint256 newPolicyId = currentPolicyId;
 
-        // ایجاد سیاست جدید و ذخیره آن در مپینگ
+        // Create new policy and store it in mapping
         policies[newPolicyId] = Policy({
             id: newPolicyId,
             patientAddress: _patientAddress,
@@ -44,15 +43,15 @@ contract PolicyToken {
             accessType: _accessType
         });
 
-        return newPolicyId; // برگرداندن آی‌دی سیاست جدید
+        return newPolicyId; // Return new policy ID
     }
 
-    // تابع برای بررسی اطلاعات سیاست
+    // Function to check policy information
     function getPolicy(uint256 policyId) public view returns (Policy memory) {
         return policies[policyId];
     }
 
-    // تابع برای بررسی اینکه آیا بیمار به دارو دسترسی دارد یا خیر
+    // Function to check whether patient has access to drug or not
     function hasAccess(address _patientAddress, uint256 _drugId, AccessType _requiredAccessType) public view returns (bool) {
         for (uint256 i = 1; i <= currentPolicyId; i++) {
             Policy memory policy = policies[i];
