eb5c4779f00a41efde7854208e30db948448fc69
Hadis Rezaei <hrezaei@unisa.it>
2025-06-04 21:28:15 +0200

    Update AccessToken.sol

--- contracts/core/AccessToken.sol
+++ contracts/core/AccessToken.sol
@@ -5,9 +5,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AccessToken is ERC721 {
-
     enum AccessType { READ, WRITE, EXEC }
-    // ساختار دسترسی
+    
+    // Access structure
     struct Access {
         uint256 id;
         string name;
@@ -15,33 +15,35 @@ contract AccessToken is ERC721 {
         uint256 endDate;
         AccessType typeAccess;
     }
-    address public owner;
-    uint256 private currentAccessId; // شمارنده برای ایجاد دسترسی‌های جدید
-    mapping(uint256 => Access) public accesses; // مپینگ برای دسترسی‌ها
 
-    // رویدادها
+    address public owner;
+    uint256 private currentAccessId; // Counter for creating new accesses
+    mapping(uint256 => Access) public accesses; // Mapping for accesses
+
+    // Events
     event DrugTracked(uint256 drugId, string status, uint256 timestamp, uint256 temperature, bool isSafe);
     event AccessGranted(uint256 accessId, address indexed grantedTo);
+
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this action");
         _;
     }
 
-     constructor() ERC721("AccessToken", "ATKN")   {
-        owner=msg.sender;
-     }
+    constructor() ERC721("AccessToken", "ATKN") {
+        owner = msg.sender;
+    }
 
-    // تابع برای ایجاد دسترسی و صدور توکن ERC721 جدید
+    // Function to create access and issue new ERC721 token
     function createAccess(
         string memory _name,
         uint256 _startDate,
         uint256 _endDate,
         AccessType _typeAccess
     ) public onlyOwner returns (uint256) {
-        currentAccessId += 1; // افزایش شمارنده
+        currentAccessId += 1; // Increment counter
         uint256 newAccessId = currentAccessId;
 
-        // ایجاد دسترسی و ذخیره آن در مپینگ
+        // Create access and store it in mapping
         accesses[newAccessId] = Access({
             id: newAccessId,
             name: _name,
@@ -50,21 +52,17 @@ contract AccessToken is ERC721 {
             typeAccess: _typeAccess
         });
 
-        // صدور توکن ERC721 به آدرس مالک
+        // Issue ERC721 token to owner address
         _safeMint(msg.sender, newAccessId);
 
-        // ارسال رویداد
+        // Send event
         emit AccessGranted(newAccessId, msg.sender);
 
-        return newAccessId; // برگرداندن آی‌دی دسترسی جدید
+        return newAccessId; // Return new access ID
     }
 
-    // تابع برای بررسی اطلاعات دسترسی
+    // Function to check access information
     function getAccess(uint256 accessId) public view returns (Access memory) {
         return accesses[accessId];
     }
-
-
-
- 
 }
